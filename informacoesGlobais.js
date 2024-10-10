84f9723e9458f2c65f7fa19017d3d843b65f77b0
Matias <107263719+Mateys13@users.noreply.github.com>
2024-10-10 11:07:49 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,16 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
+    
+    const container = document.getElementById(graficos-container)
+    container.appendChild(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
